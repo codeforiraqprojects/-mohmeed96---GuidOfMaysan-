76b9abac56615a20170f22f4890d164d212a698a
Hawraa Arkan <h.a.j.engineer@gmail.com>
2021-01-23 22:29:40 +0300

    GuidOfMaysan

---
+++ app/src/main/java/com/mohammed/guidofmaysan/RecyclerView/InfoViewHolder.java
@@ -0,0 +1,35 @@
+package com.mohammed.guidofmaysan.RecyclerView;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mohammed.guidofmaysan.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+class InfoViewHolder extends RecyclerView.ViewHolder {
+
+    //TextView personId,personAge, personName;
+    TextView name;
+    ImageView imageItem;
+    Button buttonItem;
+    CardView cardView ;
+
+    InfoViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        name=itemView.findViewById(R.id.name);
+        imageItem=itemView.findViewById(R.id.imageItem);
+        buttonItem=itemView.findViewById(R.id.bottonItem);
+      //  bottonMoreItem=itemView.findViewById(R.id.bottonMoreItem);
+         cardView=itemView.findViewById(R.id.cardMoreItem);
+
+    }
+}
